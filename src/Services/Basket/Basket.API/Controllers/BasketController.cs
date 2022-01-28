84c02df179bc48c8acab583fe87c5dfd893b9ab3
Alexander Epifanov <alexlaw00@mail.ru>
2022-01-28 19:14:33 +0300

    Add MediatR

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using MediatR;
+using Basket.API.MediatR.Commands;
 
 namespace Basket.API.Controllers
 {
@@ -19,20 +21,23 @@ namespace Basket.API.Controllers
         private readonly DiscountGrpcService _discountGrpcService;
         private readonly IPublishEndpoint _publishEndpoint;
         private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
 
-        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IPublishEndpoint publishEndpoint, IMapper mapper)
+
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IPublishEndpoint publishEndpoint, IMapper mapper, IMediator mediator)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
             _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
             _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _mediator = mediator;
         }
         
         [HttpGet("{userName}", Name = "GetBasket")]
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
         {
-            var basket = await _repository.GetBasket(userName);
+            var basket = await _mediator.Send(new GetBasketCommand(userName)); //_repository.GetBasket(userName);
             return Ok(basket ?? new ShoppingCart(userName));
         }
 
@@ -41,20 +46,22 @@ namespace Basket.API.Controllers
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
             // Communicate with Discount.Grpc and calculate lastest prices of products into sc
-            foreach (var item in basket.Items)
-            {
-                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
-                item.Price -= coupon.Amount;
-            }
+            //foreach (var item in basket.Items)
+            //{
+            //    var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+            //    item.Price -= coupon.Amount;
+            //}
 
-            return Ok(await _repository.UpdateBasket(basket));
+            //return Ok(await _repository.UpdateBasket(basket));
+            return Ok(await _mediator.Send(new UpdateBasketCommand(basket)));
         }
 
         [HttpDelete("{userName}", Name = "DeleteBasket")]        
         [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> DeleteBasket(string userName)
         {
-            await _repository.DeleteBasket(userName);
+            await _mediator.Send(new DeleteBasketCommand(userName));
+            //await _repository.DeleteBasket(userName);
             return Ok();
         }
 
